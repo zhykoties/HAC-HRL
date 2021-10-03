c046d6c59b0465bee4e4e6691d5b043e3b2585e1
Yunkai Zhang <zhykoties@gmail.com>
2021-10-03 13:09:27 -0700

    update experience replay -- reverse

--- experience_buffer.py
+++ experience_buffer.py
@@ -1,5 +1,4 @@
-from collections import deque
-import random
+import numpy as np
 
 
 class ExperienceBuffer:
@@ -7,13 +6,33 @@ class ExperienceBuffer:
     def __init__(self, max_buffer_size, batch_size):
         self.size = 0
         self.max_buffer_size = max_buffer_size
-        self.experiences = deque([], maxlen=max_buffer_size)
+        self.experiences = []
         self.batch_size = batch_size
 
     def add(self, experience):
         assert len(experience) == 7, 'Experience must be of form (s, a, r, s, g, t, grip_info\')'
         assert type(experience[5]) == bool
+
         self.experiences.append(experience)
+        self.size += 1
+
+        # If replay buffer is filled, remove a percentage of replay buffer. Only removing a single transition slows
+        # down performance
+        if self.size >= self.max_buffer_size:
+            beg_index = int(np.floor(self.max_buffer_size / 6))
+            self.experiences = self.experiences[beg_index:]
+            self.size -= beg_index
 
     def get_batch(self):
-        return [*zip(*(random.sample(self.experiences, self.batch_size)))]
+        states, actions, rewards, new_states, goals, is_terminals = [], [], [], [], [], []
+        dist = np.random.randint(0, high=self.size, size=self.batch_size)
+
+        for i in dist:
+            states.append(self.experiences[i][0])
+            actions.append(self.experiences[i][1])
+            rewards.append(self.experiences[i][2])
+            new_states.append(self.experiences[i][3])
+            goals.append(self.experiences[i][4])
+            is_terminals.append(self.experiences[i][5])
+
+        return states, actions, rewards, new_states, goals, is_terminals
